e66537c5246f6a567988d1e9dcd3294de35ebdb4
Sanjay Baro <sanjaybaro2017@gmail.com>
2023-09-27 21:22:16 +0530

    Home page initial commit

---
+++ Frontend/src/Components/Home.jsx
@@ -0,0 +1,293 @@
+import styled from "styled-components";
+import Navbar from "../Components/Navbar";
+import banner from "../Images/18733161_Business team climbing giant handshake with support of leader.jpg";
+import Footer from "./Footer";
+import logo from "../Images/DonateForChange_logo-removebg-preview.png";
+
+const Home = () => {
+  return (
+    <DIV>
+      <Navbar />
+      <div className="banner">
+        {/* <section className="hero1">
+          
+          <div className="content">
+            <h1>Donate For Change</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur.</h3>
+            <a href="#">
+              <img src={logo} />
+              Click Here
+            </a>
+          </div>
+        </section> */}
+
+        <img src={banner} alt="" />
+      </div>
+
+      {/************************   section-start  ********************* */}
+      <div className="giftSec">
+        {/* <img src={gift} alt="" /> */}
+
+        <div className="gifttext">
+          <p className="giftCapt">Lorem</p>
+          <p className="giftdesc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+            error similique aperiam magni ex modi!
+          </p>
+
+          <p className="shoText" style={{ color: "#dfdfdf" }}>
+            LOREM &nbsp;&nbsp; L O R E M
+          </p>
+        </div>
+      </div>
+
+      {/************************  about-section-start  ********************* */}
+
+      <div className="about">
+        <div className="abtMSec">
+          <div className="abtText">
+            <div className="abthead">
+              <p className="newHead-Capt">About</p>
+              <p className="newHead-desc">Donate for Chhange</p>
+            </div>
+
+            <div className="abtp">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+                beatae saepe porro distinctio quod voluptates repellat harum
+                accusamus laboriosam explicabo.
+              </p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
+                nobis rerum cum quia, doloremque quod quo aliquid maiores
+                molestiae, quae sapiente laudantium tempore?
+              </p>
+            </div>
+
+            <p className="abtp">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium quasi animi ipsum optio nostrum ab commodi pariatur
+              minima laborum dignissimos impedit reiciendis veniam quo, sit vero
+              sapiente sunt molestiae eos.
+            </p>
+          </div>
+          <div className="abtImg">{/* <img src={abtImg} alt="" /> */}</div>
+        </div>
+      </div>
+
+      {/************************  about-section-end  ********************* */}
+
+      {/********************* * start-section ***************/}
+
+      <div className="arrange">
+        <div className="arrangeSec">
+          <div className="arrimg">{/* <img src={meetImg} alt="" /> */}</div>
+          <div className="arrText">
+            <div className="abthead">
+              <p className="newHead-Capt">dsfdsf dsfds</p>
+              <p className="newHead-Capt">sdfsdf sdafsdf</p>
+
+              <p className="newHead-desc">fdgfds g gdsfg f dfg fd g gfds gf</p>
+              <br />
+              <br />
+              <br />
+              <p className="shoText" style={{ color: "#2d2c2c" }}>
+                L E A R N &nbsp;&nbsp; M O R E
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/********************* * section-end ***************/}
+
+      {/* <Footer /> */}
+      <Footer />
+    </DIV>
+  );
+};
+
+export default Home;
+
+const DIV = styled.section`
+  @import url("https://fonts.googleapis.com/css2?family=Pathway+Gothic+One&display=swap");
+  background-color: #f8f8f8;
+  .banner img {
+    width: 100%;
+  }
+  .collectSection {
+    padding: 100px 0;
+  }
+
+  /* .hero1{
+    display:flex;
+    height:100vh;
+    background-image:url("../Images/18733739_Hands holding team of tiny business persons.jpg")
+    background-size:cover;
+  }
+  /* .content{ */
+    /* display:flex;
+  flex-direction:column;
+  max-width:1200px;
+  margin:0 auto;
+  justify-content:center;
+  align-items:center;
+  text-align:center;
+  color:black; */
+  /* } */ */
+
+
+  .newHead {
+    width: 90%;
+    margin: 0 auto;
+    padding: 10px 0px 25px 10px;
+  }
+
+  .newHead-Capt {
+    font-family: "Pathway Gothic One", sans-serif;
+    font-size: 34px;
+    font-weight: 600;
+    color: #0a0a0a;
+    margin: 0;
+  }
+
+  .newHead-desc {
+    font-family: Arial, sans-serif;
+    margin: 6px 0;
+    color: #6a6a6a;
+  }
+
+
+  .categorySec {
+    padding: 50px 0;
+    background-color: #fff;
+  }
+  .catMainSec {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+  }
+
+  .catText {
+    width: 50%;
+  }
+
+  .catImg {
+    width: 50%;
+  }
+  .catImg img {
+    width: 100%;
+    height: 500px;
+    object-fit: cover;
+  }
+
+  .allcat ul {
+    list-style-type: none;
+  }
+
+  .allcat ul li {
+    list-style-type: none;
+    padding: 15px 5px;
+    font-size: 19px;
+  }
+  .allcat ul li a {
+    text-decoration: none;
+    color: #4e4e4e;
+    font-weight: 500;
+  }
+
+  /************** * about ***********************/
+  .about {
+    padding: 50px;
+    background-color: #fff;
+  }
+  .abtMSec {
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+  }
+  .abtText {
+    text-align: justify;
+    padding-top: 60px;
+    width: 50%;
+  }
+  .abtp {
+    font-size: 18px;
+    color: #282828;
+  }
+  .abtImg {
+    width: 50%;
+  }
+  /********************* meeting  **********/
+  .arrange {
+    background-color: #fff;
+    padding: 30px;
+  }
+
+  .arrangeSec {
+    background-color: #fff;
+    display: flex;
+    width: 90%;
+    margin: 0 auto;
+    justify-content: space-around;
+  }
+  .arrimg {
+    width: 50%;
+  }
+
+  .arrimg img {
+    width: 100%;
+  }
+
+  @media screen and (min-device-width: 310px) and (max-device-width: 480px) {
+    .catMainSec,
+    .abtMSec,
+    .arrangeSec {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .newCollection {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 20px;
+    }
+    .abtImg img {
+      width: 100%;
+    }
+    .giftdesc {
+      display: none;
+    }
+    .newCollection div {
+      width: 100%;
+    }
+    .collectSection {
+      padding: 70px 0;
+    }
+
+    .newHead-Capt {
+      font-family: "Pathway Gothic One", sans-serif;
+      font-size: 23px;
+      font-weight: 600;
+      color: #0a0a0a;
+      margin: 0;
+    }
+
+    .abtText {
+      padding-top: 30px;
+      width: 100%;
+    }
+    .abtp {
+      font-size: 14px;
+    }
+    .abtImg {
+      width: 100%;
+    }
+    .arrimg {
+      width: 100%;
+    }
+    .catImg {
+      width: 100%;
+    }
+  }
+`;
---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,252 @@
+import React, { useState } from "react";
+import logo from "../Images/DonateForChange_logo-removebg-preview.png";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Box } from "@chakra-ui/layout";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [colorChange, setColorchange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 200) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
+  return (
+    <DIV>
+      {/* //  */}
+      <nav className={colorChange ? "navbar colorChange" : "navbar"}>
+        <header className="header">
+          <nav>
+            <ul className="menu-items">
+              <li>
+                For Individuals
+                <div className="mega-menu">
+                  <div className="content">
+                    <div className="col">
+                      <section>
+                        <h2 className="item-1-heading">C A T E G O R I E S</h2>
+                        <ul className="mega-links">
+                          <li>
+                            <Link>Lorem</Link>
+                          </li>
+                          <li>
+                            <Link>Lorem</Link>
+                          </li>
+                          <li>
+                            <Link>Lorem</Link>
+                          </li>
+                          <li>
+                            <Link>Lorem</Link>
+                          </li>
+                          <li>
+                            <Link>Lorem</Link>
+                          </li>
+                          <li>
+                            <Link>Lorem</Link>
+                          </li>
+                        </ul>
+                      </section>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <img src={logo} />
+              </li>
+              <li> Sign Up</li>
+
+              <Box className="Login">
+                <Box>Login</Box>
+              </Box>
+            </ul>
+          </nav>
+        </header>
+
+        {/* </ul> */}
+      </nav>
+    </DIV>
+  );
+};
+
+export default Navbar;
+
+const DIV = styled.section`
+  
+   li > img { 
+    /* margin-left:50px; */
+    /* border:1px solid yellow; */
+    width: 45%;
+    margin:auto;
+  } 
+  .mega-links{
+    display:flex
+  }
+  .mobile-logo {
+    width: 60%;
+  }
+  .menu-items{
+    /* border:1px solid blue; */
+    /* display:flex */
+    align-items:center
+    justify-content:space-evenly;
+  }
+  .navbar {
+    /* display: flex; */
+    /* justify-content: space-evenly; */
+    align-items: center;
+    padding: 20px;
+    width: 100%;
+    /* border:1px solid red; */
+    position: fixed;
+    z-index:9999;
+  }
+  .colorChange {
+    background-color: #fff;
+    color: #2e2d2d;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+  .colorChange a {
+    color: #2e2d2d !important;
+  }
+
+  .navbar__logo {
+    font-weight: bold;
+    font-size: 24px;
+  }
+
+  .navbar__mobile-toggle {
+    width: 40%;
+    font-size: 24px;
+    cursor: pointer;
+    display: none; /* Hide by default */
+  }
+
+  .navbar__menu {
+    /* display: flex; */
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    justify-content: space-around;
+  }
+
+  .navbar__item {
+    margin-left: 20px;
+  }
+
+  .navbar__item a {
+    text-decoration: none;
+    color: #f3f2f2;
+  }
+
+  .navbar__item a.active {
+    border: 1px solid #8b80806b;
+    padding: 4px 7px 4px 7px;
+  }
+
+  .mobile-menu-open {
+    display: block;
+    /* border: 2px solid red; */
+  }
+
+  .Login {
+    display: none;
+  }
+  /* Initially hide the menu on larger screens */
+  @media (max-width: 767px) {
+    .menu-list.mobile-menu-open {
+      display: block;
+    }
+  }
+
+  @media (max-width: 768px) {
+    .col_1 {
+      display: none;
+    }
+    .Profile {
+      display: none;
+    }
+    .Login {
+      display: block;
+    }
+    .navbar {
+      flex-direction: column;
+      align-items: flex-start;
+      /* bor
+      der: 2px solid red; */
+    }
+
+    .navbar__menu {
+      display: flex;
+      flex-direction: column;
+      background-color: #2a2929;
+      padding: 10px;
+      transition: transform 0.3s ease-in-out;
+      /* transform: translateY(-50%); */
+      opacity: 0;
+      position: absolute;
+      top: 70px;
+      left: 0;
+      width: 100%;
+      z-index: 1;
+      color: #fff;
+    }
+
+    .navbar__menu.active {
+      transform: translateY(0);
+      opacity: 1;
+    }
+
+    .navbar__mobile-toggle {
+      display: block;
+    }
+  }
+  @media screen and (min-device-width: 310px) and (max-device-width: 480px) {
+    .navbar__logo {
+      display: none;
+    }
+    .navbar__item {
+      margin-left: 20px;
+      text-align: center;
+      border-bottom: 1px solid #3c3a3a52;
+      padding: 5px 0;
+    }
+
+    .mobileNav {
+      width: 100%;
+      display: flex;
+      /* border: 2px solid red; */
+    }
+    .colorChange a {
+      color: #ddd9d9 !important;
+    }
+  }
+  @media screen and (min-device-width: 481px) and (max-device-width: 768px) {
+    .navbar__logo {
+      display: none;
+    }
+    .navbar__item {
+      margin-left: 20px;
+      text-align: center;
+      border-bottom: 1px solid #3c3a3a52;
+      padding: 5px 0;
+    }
+    .mobileNav {
+      width: 100%;
+      display: flex;
+    }
+    .colorChange a {
+      color: #ddd9d9 !important;
+    }
+  }
+  @media screen and (min-device-width: 769px) {
+    .mobile-logo {
+      display: none;
+    }
+  }
+`;
