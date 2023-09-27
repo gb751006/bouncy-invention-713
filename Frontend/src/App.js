b4fe684a41d28792d4e66e4889040c145ea204d6
Sanjay Baro <sanjaybaro2017@gmail.com>
2023-09-27 22:22:07 +0530

    Home Page inital commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
-     <h1>Donate for Change</h1>
+      {/* <h1>Donate for Change</h1> */}
+      <Home />
     </div>
   );
 }
